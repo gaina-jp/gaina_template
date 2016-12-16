82ba1d5916dc0e5abd6ad148443f3e46d71e1712
GAINA <nagaitakasumi@gmail.com>
2016-12-16 17:27:35 +0900

    20161216

--- libs/__startProject.bat
+++
@@ -1,3 +0,0 @@
-cd /d %~dp0
-node dirs.js
-npm install
--- libs/dirs.js
+++
@@ -1,1 +0,0 @@
-var f=require("fs"),a=["sass","jade","coffee","typescript","../dev","../production","test","stylus"],i=0,l=a.length;for(;i<l;i++){f.mkdir(a[i])}
